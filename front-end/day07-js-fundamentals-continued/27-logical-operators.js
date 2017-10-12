f2cf62d07dc5f056da021f248bbbc28ffd19bf94
Farhad Ahmed <farhadahmed@Farhads-MacBook-Pro.local>
2017-10-11 23:52:56 -0700

    Remove uncovered lessons and update lessons that were covered.

--- front-end/day07-js-fundamentals-continued/27-logical-operators.js
+++ front-end/day07-js-fundamentals-continued/27-logical-operators.js
@@ -1,36 +1,41 @@
-// Logical operators can be used with boolean values (or with using comparison operators
-// to create boolean values)
-
-&& // This operator means 'and'
-|| // This operator means 'or'
-!  // This operator means 'not'
-
-(true) && (true)  // && returns true if both values on left and right equate to true
-//true
-
-(true) && (false)
-//false
-
-(true) && (!false)
-//true
-
-(true) || (false) // || returns true if EITHER of the values on the left or right are true.
-				  // If the value on the left returns true, the compiler won't evaulate
-				  // the value on the right since the result is already known to be true.
-//true
-
-(false) || (false)
-//false
-
-(true) || (true)
-//true
-
-(2 > 1) && (4 === 4) // This will evaluate as true since both sides are true
+'use strict';
+//    Logical operators can be used with boolean values (or with using comparison 
+//	  operators to create boolean values)
+/*
+      Logical operators are binary operators just like arithmetic operators.
+      Arithmetic operators: +, -, *, /, %
+      Logical operators: &&, ||, !
 
 
-(true) && !(3 > 5)
+&&    This operator means 'and'
+||    This operator means 'or'
+!     This operator means 'not'
+
+*/
 
 
+// && returns true only if BOTH values on left and right equate to true
 
-//  Create two statements using logical operators
-    //  - use two 4 different boolean operators for comparison
+console.log((true) && (true));    //true
+console.log((true) && (false));   //false
+console.log((true) && (!false));  //true
+
+
+// || returns true if EITHER of the values on the left or right are true.
+console.log((true) || (false));   //true
+console.log((false) || (false));  //false
+console.log((true) || (true));    //true
+
+console.log((2 > 1) && (4 === 4));  // This will evaluate as true since both sides are true.
+console.log((true) && !(3 > 5));    // This will evaluate to false since the value on the
+                                    // right is false.
+
+
+// Logical operators ask a question:
+
+// Is it the case that both these values on the left and right are true? 
+true && false // false
+
+// Is it the case that EITHER of these are true?
+true || false // true
+false || false // false
