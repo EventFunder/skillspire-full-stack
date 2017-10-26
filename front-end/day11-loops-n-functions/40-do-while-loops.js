7a5ffdf2950cf4ed8199c3d26bc2a804c4ffa796
Farhad Ahmed <farhadahmed@Farhads-MacBook-Pro.local>
2017-10-26 01:55:39 -0700

    update lessons for day 11

--- front-end/day11-loops-n-functions/40-do-while-loops.js
+++ front-end/day11-loops-n-functions/40-do-while-loops.js
@@ -1,36 +1,43 @@
 'use strict';
 
-// Work similar to a while loop. Has one difference.
-// It starts with the task, and THEN checks the condition.
+// quick review of while loops
+// create while loop that logs 0-10
+
+var num = 0;
+
+while (num <= 10) {
+	console.log(num);
+	num++;
+}
+
+
+// A do-while loop works similar to a while loop.
+// Difference:
+// While loop checks a condition and then runs task.
+// Do-while loop runs task first and then checks 
+// the condition.
 
 // syntax
+/*
 do {
 	//some task
 } while (condition);
+*/
 
 
-// Below we will use a do-while loop to log numbers 0 to 5 just so you can see the
+// Below we will use a do-while loop to log numbers 0 to 10 just so you can see the
 // difference between a while loop and a do-while loop.
 var num = 0;
 
 do {
 	console.log(num);
 	num++
-} while (num <= 5);
+} while (num <= 10);
 
 
-// Example of a use case for do-while loops. We'll prompt the user to choose a username.
+// Example of a use case for do-while loops: We'll prompt a user to input a username.
 // As long as the user doesn't type anything, we will continue to prompt him.
 
 do {
 	var username = prompt('Choose a username');
 } while (username === '');
-
-
-
-
-
-var answer = 7;
-do {
-	var userGuess = prompt('Guess the number I chose.');
-} while (userGuess != answer);
--- front-end/day11-loops-n-functions/41-for-loops.js
+++ front-end/day11-loops-n-functions/41-for-loops.js
@@ -1,39 +1,62 @@
 'use strict';
 
-// A while loop is used if you don't know how long a condition will be true.
-// A for loop is used if you already know how long a condition will be true.
+// 1 - The purpose of a loop is to perform a trask
+//     multiple times.
+// 2 - Each type of loop works differently
+//	   2a - While loop checks a condition and then
+//		    performs a task if the condition is true
+//	   2b - Do-while loop performs the task and then
+//			checks the condition.
+// 3 - Loops are often used to iterate through arrays
 
-// Here is the syntax for a for loop
-//Index is the count. As long as condition is true, the loop will run.
-for(index; condition; increment) {  
 
-	//task
+// while
+var num1 = 0;
+while (num1 <= 10) {
+	console.log(num1);
+	num1++;
 }
 
-// Here's how you would log numbers 0 to 5 just like in previous examples.
-// Take note of which loop takes up the fewest lines to accomplish a task.
-// This is a big part of "KISS" (Keep It Simple Stupid).
+// do-while
+var num2 = 0;
+do {
+	console.log(num2);
+	num2++;
+} while(num2 <= 10);
 
-for(var index=0; index <= 5; index++) {
-	console.log(index);
+
+
+
+// Here is the syntax for a for loop. Index is the count. As long as condition is true, the loop will run.
+/*
+
+	for (count; condition; increment) {
+		// some task
+	}
+
+*/
+
+// Let's use a for loop to log numbers 0-10 just as we did with the previous loops. Notice how it's shorter.
+for (var i = 0; i <= 10; i++) {
+	console.log(i);
 }
-
 
 
 // Use contact list as example.
 // Here's how it was done with a while loop:
-var contactList = ['Jon Snow', 'Arya', 'Cirsi', 'Tyrian', 'Greyworm'];
+//				   0		1 		 2 			3
+var contacts = ['Ahmed', 'Abdi', 'Mohamed', 'Keyse'];
 
-var count2 = 0;
-while (count2 < contactList.length) {
-	document.write('<p>' + contactList[count2] + '</p>');
-	count2++;
+var count = 0;
+while (count < contacts.length) {
+	document.write('<p>' + contacts[count] + '</p>');
+	count++;
 }
 
 
 // Here it is with a for loop.
-for (var i = 0; i < contactList.length; i++) {
-	document.write('<p>' + contactList[count2] + '</p>');
+for (var i = 0; i < contacts.length; i++) {
+	document.write('<p>' + contacts[i] + '</p>');
 }
 
 
@@ -51,9 +74,3 @@ for (var i = 0; i < contactList.length; i++) {
 
 */
 
-var str = '#';
-
-for(var i=0; i<=5; i++) {
-	console.log(str);
-	str += '#'; // str = str + '#'
-}
