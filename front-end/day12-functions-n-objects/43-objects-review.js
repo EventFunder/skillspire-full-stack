ab185315ebfbe7d987fad92a847709004627ee2a
Farhad Ahmed <farhadahmed@Farhads-MacBook-Pro.local>
2017-10-28 09:00:12 -0700

    add lesson on objects

---
+++ front-end/day12-functions-n-objects/43-objects-review.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*
+	What is an object in the real world?  A chair, a table, a book. These are objects. In programming, 
+	an object is a collection of properties that are tied to a variable. This is similar to how we
+	define an object in the real world. What is a table? It's a thing with a flat surface, has one or 
+	more legs, and is stable enough that we can place things on it (these are all properties that define 
+	a table).
+*/
+
+// An object is a data type just like a string, number, boolean, and function.
+
+// The marshawn object has 3 properties
+var marshawn = {
+	name: 'Marshawn Lynch',
+	heightInInches: 71,
+	occupation: 'American football running back',
+	location: 'Seattle',
+	// changeLocation: function(newLocation) {
+	// 	this.location = newLocation;
+	// }
+}
+
+
+// dot notation and bracket notation are used to access properties of objects.
+console.log(marshawn.name); // dot notation
+console.log(marshawn['occupation']); // bracket notation
+
+
+// To access an object's method, dot notation must be used. For this reason, developers rarely use bracket notation
+// since dot notation allows them to keep their code consistent.
+marshawn.newLocation('San Francisco');
+
+
+// We've already worked with other objects with methods attached to them.
+console.log()
+document.write()
---
+++ front-end/day12-functions-n-objects/44-loops-review.js
@@ -0,0 +1,42 @@
+// I have an array of numbers. Log the sum of those numbers.
+
+var arr = [1, 2, 3];
+
+var sum = 0;
+for (var i=0; i<arr.length; i++) {
+	sum += arr[i];
+}
+
+
+
+// Jon and Sam are competitive speakers. They're rated in 3 categories: argument strength, clarity, and originality. In each category,
+// they're given a rating of 1-10.
+
+
+var jonPoints = [3, 5, 9];
+var samPoints = [5, 2, 7];
+
+function pointComparison(arr1, arr2) {
+	var arr1Total = 0;
+	var arr2Total = 0;
+
+	for (var i=0; i<3; i++) {
+		if (arr1[i] > arr2[i]) {
+			arr1Total++;
+		} else if (arr2[i] > arr1[i]) {
+			arr2Total++;
+		}
+	}
+
+	if (jonPoints > samPoints) {
+		console.log('Jon wins.');
+	} else if (samPoints > jonPoints) {
+		console.log('Sam wins.');
+	} else {
+		console.log('Tie');
+	}
+}
+
+
+
+// Given an array of numbers, log the largest number.
