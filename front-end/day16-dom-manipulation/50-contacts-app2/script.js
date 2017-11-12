2cbb2b2daf4a9d051d7dbe169fec17f611a2966c
Farhad Ahmed <farhadahmed@Farhads-MacBook-Pro.local>
2017-11-11 20:32:34 -0800

    update lessons covered today

--- front-end/day16-dom-manipulation/50-contacts-app2/script.js
+++ front-end/day16-dom-manipulation/50-contacts-app2/script.js
@@ -6,7 +6,7 @@ var contactNumberInput = document.getElementById('contact-number-input');
 var contactEmailInput = document.getElementById('contact-email-input');
 var contactTableBody = document.getElementById('contact-table-body');
 
-
+// Object constructor to create new contact objects on form submission
 function Contact(name, number, email) {
 	this.name = name;
 	this.number = number;
@@ -23,6 +23,13 @@ function Contact(name, number, email) {
 	var newContact = new Contact(contactNameInput.value, contactNumberInput.value, contactEmailInput.value);
 
 
+	/*
+		Application Logic:
+		- Create object constructor for contacts (outside of the event listener)
+		- When user clicks submit:
+			- We will create a new contact object
+			- We will push that object into the <ul>
+	*/
 	contactTableBody.innerHTML += 
 	'<tr>' +
 		'<td>' + newContact.name + '</td>' +
