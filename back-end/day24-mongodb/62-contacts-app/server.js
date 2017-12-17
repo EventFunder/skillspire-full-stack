76a6872a9349f31aae61bd862f19f5def1a4de3a
Farhad K. Ahmed <farhadahmed2007@gmail.com>
2017-12-17 10:42:39 -0800

    update day24 lesson to reflect what was completed

--- back-end/day24-mongodb/62-contacts-app/server.js
+++ back-end/day24-mongodb/62-contacts-app/server.js
@@ -4,10 +4,6 @@ var port = 3000;
 
 app.use(express.static(__dirname + '/app/'));
 
-app.use(function(request, response, next) {
-	response.status(404).sendFile(__dirname + '/app/404.html');
-});
-
 app.listen(port, function() {
 	console.log('server starting on port ' + port);
 }); 
