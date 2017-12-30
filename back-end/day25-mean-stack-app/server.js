d5f5e29a03b4736914c747bac760b82175ae2431
Farhad K. Ahmed <farhadahmed2007@gmail.com>
2017-12-30 07:49:57 -0800

    added post request for day 26 lesson

--- back-end/day25-mean-stack-app/server.js
+++ back-end/day25-mean-stack-app/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('contactsdbforrepo', ['contactsdbforrepo']);
+var bodyParser = require('body-parser');
 var port = 3000;
 
 app.use(express.static(__dirname + '/app/'));
+app.use(bodyParser.json());
 
 app.get('/contacts', function (request, response) {
 	console.log('GET request received on server.');
@@ -14,6 +16,15 @@ app.get('/contacts', function (request, response) {
 		response.json(documents);
 	});
 	
+});
+
+app.post('/contacts', function (request, response) {
+	console.log(request.body);
+
+	// insert() sends data into the db and the response gets sent back to controller
+	db.contactsdbforrepo.insert(request.body, function (error, document) {
+		response.json(document); 
+	});
 });
 
 app.listen(port, function() {
