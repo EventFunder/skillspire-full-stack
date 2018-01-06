4021e83414bfe4da31fb03544786d15d3ed5fe4a
Farhad K. Ahmed <farhadahmed2007@gmail.com>
2018-01-06 09:14:50 -0800

    add lesson 28

---
+++ back-end/day28-whiteboarding-resumes/64-whiteboarding.js
@@ -0,0 +1,33 @@
+/*
+Create a function that takes two numbers as parameters and returns the lesser number
+*/
+
+/*
+Create a function that takes an array of numbers and logs each number
+*/
+
+/*
+Create a function that takes an array of numbers and logs the lowest number
+*/
+
+/*
+Create a function that takes a string and turns it into an array
+*/
+
+/*
+Check if string is palendrome
+*/
+
+/*
+Create a function that takes a contact object and checks if the contact has a first name.
+*/
+
+/*
+Create a function that takes a name and returns "Welcome" if the name is either
+'Bruce' or 'Rick'
+*/
+
+/*
+Create a function that takes a first and last name and returns "Welcome" if the first
+name is Rick and the last name is Grimes.
+*/
